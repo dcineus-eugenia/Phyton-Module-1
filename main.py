db328afbcb957c8be186210da57d72b5af6d4400
dcineus@eugeniaschool.com <mlarboul@student.42.fr>
2025-10-02 13:12:52 +0000

    initial commit01

---
+++ main.py
@@ -0,0 +1,43 @@
+# Exercice 1
+# Age = 22
+# Height = 1.78
+# Name = "Dcineus"
+# is_student = True
+# print(is_student,Name,Age,Height)
+# Exercice 2
+# a = 100
+# b = 200
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# reminder = (a/b)
+# print(reminder)
+# exponentiation = (a**b)
+# print(exponentiation)
+# Exercice 3
+# First_name = 'Damien'
+# Last_name = 'Cineus'
+# Full_name = (First_name+" "+Last_name)
+# print(Full_name.upper())
+# print(len(Full_name))
+# print(Full_name.split())
+# 
+# Exercice 4
+# num_str = "123"
+# num_int = int(num_str)
+# print(num_int)
+# print(type(num_int))
+# # 
+# num_float = 12.3
+# num_float_to_int = int(num_float)
+# print(num_float_to_int)
+
+# num_int = 456
+# num_int_to_float = float(num_int)
+# print(num_int_to_float)
+# # 
+# num_float = 123,4
+# num_float_to_str = str(num_float)
+# print(num_float_to_str)
+# Exercice 5
